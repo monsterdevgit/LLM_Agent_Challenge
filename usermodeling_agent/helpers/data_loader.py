473c64beba9365022770cef8facae9f446196f2a
Kenneth Essien <Kenneth.Essien@sterling.ng>
2026-05-24 20:12:28 +0100

    initial commit

--- usermodeling_agent/helpers/data_loader.py
+++ usermodeling_agent/helpers/data_loader.py
@@ -1,13 +1,28 @@
 import pandas as pd
+import kagglehub
+import os
 
 def load_datasets():
 
-    # ✅ Load CSV
+    # Load user personality CSV
     users_df = pd.read_csv("datasets/analysis.csv")
 
-    # ✅ Load JSONL (Amazon dataset)
+    # Download latest Amazon reviews dataset
+    path = kagglehub.dataset_download(
+        "hakim11/cell-phones-and-accessories-5"
+    )
+
+    print("Path to dataset files:", path)
+
+    # Build JSON file path
+    json_path = os.path.join(
+        path,
+        "Cell_Phones_and_Accessories_5.json"
+    )
+
+    # Load JSONL dataset
     reviews_df = pd.read_json(
-        "datasets/Cell_Phones_and_Accessories_5.json",
+        json_path,
         lines=True
     )
 
